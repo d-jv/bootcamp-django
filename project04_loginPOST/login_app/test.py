2141fdf3d6b44ae570a3005b428d82e709bc8976
diego-jv <diego.jeria@usach.cl>
2021-07-31 10:17:06 -0400

    commit before disaster 2

---
+++ project04_loginPOST/login_app/test.py
@@ -0,0 +1,3 @@
+data = []
+
+data.append(dict({1: 'olakease', 2:'olakease2'}))
--- project04_loginPOST/login_app/views.py
+++ project04_loginPOST/login_app/views.py
@@ -4,26 +4,32 @@ from django.shortcuts import redirect, render, HttpResponse
 def index(request):
     return redirect ('/login')
 
+data = []
 def loginPage(request): 
-    data = {
-    }
     if request.method == 'GET':
-        return render(request, 'index.html', data)
+        return render(request, 'index.html')
     else:
+        #user = next((u for u in USERS if u['nombre'] == name_from_form), None)
         print('Username: ', request.POST['username'])
         print('Password: ', request.POST['password'])
-        username_form = request.POST['username']
-        password_form = request.POST['password']
-        request.session['username'] = username_form
-        request.session['password'] = password_form
-        return redirect('/success', data)
+        # username_form = request.POST['username']
+        # password_form = request.POST['password']
+        # request.session['username'] = username_form
+        # request.session['password'] = password_form
+        data.append(dict({request.POST['username']:request.POST['password']}))
+        request.session['username'] = request.POST['username']
+        request.session['password'] = request.POST['password']
+        print('---------------Antes de mostrar el Data------------')
+        print(data)
+        print('---------------Despues de mostrar el Data------------')
+        return redirect('/success')
 
 def success(request):
-    data = {
+    context = {
         'username' : request.session['username'],
         'password' : request.session['password']
     }
-    return render(request, 'success.html', data)
+    return render(request, 'success.html', context)
 
 def logout(request): 
     del request.session['username']
