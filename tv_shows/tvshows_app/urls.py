00cc4adf13481b109355d9e5a8b60da52dd3636d
diego-jv <diego.jeria@usach.cl>
2021-08-18 22:02:03 -0400

    added edit, update and show pages

--- tv_shows/tvshows_app/urls.py
+++ tv_shows/tvshows_app/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('shows', views.shows),
+    path('shows/<int:id>', views.show),
+    path('shows/<int:id>/edit', views.edit),
+    path('shows/<int:show_id>/update', views.update),
     path('shows/new', views.newShow),
     path('add_show', views.addShow),
     # path('remove/<int:network_id>/<int:show_id>', views.remove),
--- tv_shows/tvshows_app/views.py
+++ tv_shows/tvshows_app/views.py
@@ -5,6 +5,24 @@ from .models import *
 def index(request):
     return redirect('/shows')
 
+
+
+def show(request, id):
+    show = Show.objects.get(id=id)
+    data = {
+        'show' : show
+    }
+    return render(request, 'show.html', data)
+
+def edit(request, id):
+    show = Show.objects.get(id=id)
+    time_str = show.release_date.strftime('%Y-%m-%d')
+    data = {
+        'show' : show,
+        'time_str' : time_str
+    }
+    return render(request, 'edit.html', data)
+
 def shows(request):
     data = {
         'shows': Show.objects.all()
@@ -45,4 +63,26 @@ def remove(request, show_id):
     # print(show)
     #Network.shows.remove(show)
     show.delete()
+    return redirect(request.META.get('HTTP_REFERER'))
+
+def update(request, show_id):
+    if request.POST['network_id'] == 'other':
+        # en este caso hay que crearla
+        new_network_name = request.POST['newNetwork']
+        network = Network.objects.create(name = new_network_name)
+    else:
+        #pescar lo que venga del network id y traer el network
+        # en este caso hay que rescatarla de la DB
+        network_id = request.POST['network_id']
+        network = Network.objects.get(id=network_id)
+    # -------
+    show = Show.objects.get(id=int(show_id))
+    showTitle = request.POST['title']
+    showDate = request.POST['date']
+    showDesc = request.POST['desc']
+    show.title = showTitle
+    show.release_date = showDate
+    show.desc = showDesc
+    show.networks.add(network)
+    show.save()
     return redirect(request.META.get('HTTP_REFERER'))
